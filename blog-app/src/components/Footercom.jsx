d305ed3ae92bd8d750d3b49f179770c293dd1dda
Anshul <ananyaj138@gmail.com>
2024-05-22 12:50:17 +0530

    "footer committed"

--- blog-app/src/components/Footercom.jsx
+++ blog-app/src/components/Footercom.jsx
@@ -1,60 +1,67 @@
-import { Footer } from 'flowbite-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { data } from "autoprefixer";
+import { Footer } from "flowbite-react";
+import React from "react";
+import { BsFacebook, BsGithub, BsInstagram, BsLinkedin, BsTwitter } from "react-icons/bs";
+import { Link } from "react-router-dom";
 export default function Footercom() {
   return (
-    <Footer className='border-b-t-8 boorder-teal-500'>
-      <div className=''>
-        <div className=''>
-          <div className=''>
-          <Link to="/ " className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Tagline</span>
-        Blog
-      </Link>
+    <Footer className="border border-t-8 border-teal-500">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full lg:gap-16 justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5 ">
+            <Link
+              to="/ "
+              className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                Tagline
+              </span>
+              Blog
+            </Link>
           </div>
-          <div className='grid grid-cols-2 gap-3 sm: mt-4 sm:grid-cols-3 sm:gap-6'>
+          <div className="grid grid-cols-2 gap-2 mt-4 sm:grid-cols-3 sm:gap-6">
             <div>
-    <Footer.Title title='About'/>
-    <Footer.LinkGroup col>
-      <Footer.Link href='/'>
-        Node projects
-      </Footer.Link>
-      <Footer.Link href='/'>
-        Tagline blogs
-      </Footer.Link>
-    </Footer.LinkGroup>
-          </div>
-          
-          <div className='grid grid-cols-2 gap-3 sm: mt-4 sm:grid-cols-3 sm:gap-6'>
-            <div>
-    <Footer.Title title='Follow Us'/>
-    <Footer.LinkGroup col>
-      <Footer.Link href='/'>
-        Github
-      </Footer.Link>
-      <Footer.Link href='/'>
-        linkedIn
-      </Footer.Link>
-    </Footer.LinkGroup>
-    </div>
-   
-    <div className='grid grid-cols-2 gap-3 sm: mt-4 sm:grid-cols-3 sm:gap-6'>
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="/">Node projects</Footer.Link>
+                <Footer.Link href="/">Tagline blogs</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
 
-    <Footer.Title title='Legal'/>
-    <Footer.LinkGroup col>
-      <Footer.Link href='/'>
-        Privcy policy
-      </Footer.Link>
-      <Footer.Link href='/'>
-        Terms $ Conditions
-      </Footer.Link>
-    </Footer.LinkGroup>
-          </div>
-          </div>
+            <div className="grid grid-cols-2 gap-3 sm: mt-4 sm:grid-cols-3 sm:gap-6">
+              <div>
+                <Footer.Title title="Follow Us" />
+                <Footer.LinkGroup col>
+                  <Footer.Link href="/">Github</Footer.Link>
+                  <Footer.Link href="/">linkedIn</Footer.Link>
+                </Footer.LinkGroup>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 gap-3 sm: mt-4 sm:grid-cols-3 sm:gap-6">
+              <div>
+                <Footer.Title title="Legal" />
+                <Footer.LinkGroup col>
+                  <Footer.Link href="/">Privcy policy</Footer.Link>
+                  <Footer.Link href="/">Terms $ Conditions</Footer.Link>
+                </Footer.LinkGroup>
+              </div>
+            </div>
+            
+            <div className="w-full sm:flex sm:items-center sm:justify-between">
+              <Footer.Copyright href="#" by='Taglines blog' year={new Date().getFullYear()}/>
+              <div className="flex gap-4 sm:mt-0  mt-4 sm:justify-center">
+                <Footer.Icon href="#" icon={BsFacebook}/>
+                <Footer.Icon href="#" icon={BsInstagram}/>
+                <Footer.Icon href="#" icon={BsTwitter}/>
+                <Footer.Icon href="#" icon={BsGithub}/>
+                <Footer.Icon href="#" icon={BsLinkedin}/>
+
+              </div>
+
+            </div>
           </div>
         </div>
       </div>
-</Footer>)
-
-
-  }
+    </Footer>
+  );
+}
